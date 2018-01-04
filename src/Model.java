3d3fa10ab35648cfe1725ca32573374d0e2d0bc3
chorin <chorin@gmail.com>
2018-01-04 10:18:05 +0200

    more work on Model

--- src/Model.java
+++ src/Model.java
@@ -1,14 +1,41 @@
+import java.util.ArrayList;
+
 public class Model {
 	private Board board;
+	ArrayList possibleMovesP1 = new ArrayList<Pos>();
+	ArrayList possibleMovesP2 = new ArrayList<Pos>();
 
-
-
+	
 
 	Model(int boardSize) {
 
 	}
 
-	boolean place (Player player, int x, int y) {
+	public boolean place (Player player, Pos pos) {
 
+	}
+
+	public boolean isPossibleMove(Player player, Pos pos) {
+
+	}
+
+	public int calcScoreOf(Player player) {
+
+	}
+
+
+	private boolean goTo(Direction direction, Cell currPlayerPiece, Pos pos, boolean doFlip, boolean found) {
+
+	}
+
+	private enum Direction {
+		NORTH,
+		SOUTH,
+		WEST,
+		EAST,
+		NW,
+		NE,
+		SE,
+		SW
 	}
 }
